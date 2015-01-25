645abec7ad99ccf8ac7fa3fbd856e78aa4698c9d
MarshalJoe <joseph.charles.marshall@gmail.com>
2015-01-24 23:48:37 -0600

    Adds error handling for broken URLs

--- crawler.js
+++ crawler.js
@@ -9,77 +9,81 @@ var links = [];
 
 var openStream = fs.open(file, 'r');
 
+
+console.log("The Friendly Ghost is a PR-bot that scrapes and saves email addresses into an emails.txt file. Sites to search:");
+
+// read, log, and save links in an array
 while(!openStream.atEnd()) {
     var line = openStream.readLine();
     links.push(line);
-}
-
-console.log("Sites to be searched:");
-
-for (var l=0; l < links.length; l++) {
-	console.log(links[l]);
+    console.log(line);
 }
 
 
 casper.start().each(links, function (self, link) {
-	self.thenOpen(link, function() {
-		// print website title
-		this.echo('Searching ' + this.getCurrentUrl());
-
-		// GRAB EMAILS FROM LINKS
-		// grab the "href" HTML attribute contents of every link and put them into an array
-		var linkArray = this.getElementsAttribute('a', 'href');
-		
-		// regular expression for matching emails
-		var emailRegex = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
-
-		// Array containing the emails found
-		var emailArray = [];
-
-		// loop through every href, searching for an email address
-		for(var i=0; i <= linkArray.length; i++) {
+		self.thenOpen(link, function() {
 			
-			var attribute = linkArray[i];
-			
-			// if it finds an email, save it
-			if (emailRegex.test(attribute)) {
-				var email = attribute;
-				// first stripping any 'mailto's
-				if (attribute.substring(0,7) === 'mailto:'); {
-					email = attribute.substring(7);
+			// GRAB EMAILS FROM LINKS AND HTML
+			// grab the "href" HTML attribute contents of every link and put them into an array
+			try {
+			var linkArray = this.getElementsAttribute('a', 'href');
+			var text = this.getElementsInfo('p');
+			this.echo('Searching ' + this.getCurrentUrl());
+
+			} catch (e) {
+				var text =[];
+				var linkArray =[];
+				console.log("Problem accessing page " + link + ' ' + e);
+				return;
+			}
+
+			// regular expression for matching emails
+			var emailRegex = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+
+			// Array containing the emails found
+			var emailArray = [];
+
+			// loop through every href, searching for an email address
+			for(var i=0; i <= linkArray.length; i++) {
+				
+				var attribute = linkArray[i];
+				
+				// if it finds an email, save it
+				if (emailRegex.test(attribute)) {
+					var email = attribute;
+					// first stripping any 'mailto's
+					if (attribute.substring(0,7) === 'mailto:'); {
+						email = attribute.substring(7);
+					}
+					emailArray.push(email);
 				}
-				emailArray.push(email);
+
 			}
 
-		}
-
-		//GRAB EMAILS FROM HTML TEXT
-		var text = this.getElementsInfo('p');
-
-		// check text snippets for emails, then save
-		for(var k=0; k < text.length; k++) {
-			var elementText = text[k].text
-			if (emailRegex.test(elementText)) {
-				var emailMatches = elementText.match(emailRegex);			
-				emailArray.push(emailMatches[0]);
+			// check text snippets for emails, then save
+			for(var k=0; k < text.length; k++) {
+				var elementText = text[k].text
+				if (emailRegex.test(elementText)) {
+					var emailMatches = elementText.match(emailRegex);			
+					emailArray.push(emailMatches[0]);
+				}
 			}
-		}
 
-		// print out and write to file all the emails discovered for that url 
-		if (emailArray.length == 0) { 
-			console.log("No emails found on this page.");
-		} else {
-			var downloadStream = fs.open('emails.txt', 'w');
-			for (email in emailArray) {
-				downloadStream.writeLine(emailArray[email]);
-				console.log(emailArray[email]);
+			// print out and write to file all the emails discovered for that url 
+			if (emailArray.length == 0) { 
+				console.log("No emails found.");
+			} else {
+				var downloadStream = fs.open('emails.txt', 'w');
+				for (email in emailArray) {
+					downloadStream.writeLine(emailArray[email]);
+					console.log(emailArray[email]);
+				}
+				downloadStream.flush();
+				downloadStream.close();
 			}
-			downloadStream.flush();
-			downloadStream.close();
-		}
 
 
-	});
+		});
 
 });
 
